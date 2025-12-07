9b39f5b0c257375f626d6270a2e9567c1fcb7c84
Philosoph228 <philosoph228@gmail.com>
2025-12-07 23:36:37 +0500

    Disasm: add main dialog frame

--- msvc_main.cpp
+++ msvc_main.cpp
@@ -1,13 +1,44 @@
 #include "precomp.hpp"
-
 #include "Disasm.h"
-
 #include <iostream>
+#include "resource.h"
 
 BYTE* Code;
 CRITICAL_SECTION g_cs;
 
 static constexpr int CODE_SIZE = 4096;
+
+INT_PTR CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        // Initialization goes here
+        return TRUE;
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+        case IDCANCEL:
+            // close the dialog (main window)
+            DestroyWindow(hWnd);
+            return TRUE;
+        }
+        break;
+
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        return TRUE;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
 
 void AllocCode()
 {
@@ -16,6 +47,11 @@ void AllocCode()
 
 int main(int argc, char* argv[])
 {
+	HINSTANCE hInst = ::GetModuleHandle(nullptr);
+
+	HWND hDialog = ::CreateDialogParamW(hInst, MAKEINTRESOURCE(IDD_DIALOG1), nullptr, static_cast<DLGPROC>(&DlgProc), 0);
+	::ShowWindow(hDialog, SW_SHOW);
+
 	::InitializeCriticalSection(&g_cs);
 
 	std::cout << "IDR MSVC Started\n";
@@ -35,6 +71,17 @@ int main(int argc, char* argv[])
 	::DeleteCriticalSection(&g_cs);
 
 	std::cout << "Instruction length: " << instrLen << "\nDisassembled line: " << disLine << '\n';
+
+    MSG msg{};
+    while (::GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!IsDialogMessage(hDialog, &msg)) {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+        }
+    }
+
+    return static_cast<int>(msg.wParam);
 	
 	return 0;
 }
