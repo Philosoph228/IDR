957ea5224a6f66efd34ec59ad5bc9cadc7d8d69d
Philosoph228 <philosoph228@gmail.com>
2025-12-07 14:53:56 +0500

    Disasm: copy SegmentInfo from Main.cpp
    * TList to std::vector<void*> for SegmentList global variable
    * update Adr2Pos implementation in accordance with updated data types

--- Util.cpp
+++ Util.cpp
@@ -1,12 +1,14 @@
 #include "precomp.hpp"
 #include "Util.hpp"
 
+std::vector<void*> SegmentList;
+
 int __fastcall Adr2Pos(DWORD adr)
 {
     int     ofs = 0;
-    for (int n = 0; n < SegmentList->Count; n++)
+    for (auto& segment : SegmentList)
     {
-        PSegmentInfo segInfo = (PSegmentInfo)SegmentList->Items[n];
+        PSegmentInfo segInfo = (PSegmentInfo)segment;
         if (segInfo->Start <= adr && adr < segInfo->Start + segInfo->Size)
         {
             if (segInfo->Flags & 0x80000)
--- Util.hpp
+++ Util.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+extern std::vector<void*> SegmentList;
+
+typedef struct
+{
+	DWORD Start;
+	DWORD Size;
+	DWORD Flags;
+	std::string Name;
+} SegmentInfo, *PSegmentInfo;
+
 int __fastcall Adr2Pos(DWORD adr);
